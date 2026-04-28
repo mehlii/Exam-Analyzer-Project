8e7947e3e50b2d2e0660721aad586a416a711bed
güler şenel <gulersenel0@gmail.com>
2026-04-28 23:30:49 +0300

    Add account URLs to urlpatterns for login/logout/register functionality

--- exam_analyzer/urls.py
+++ exam_analyzer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Türkçe not: Hesap işlemleri (login/logout/register) URL'leri
+    path("accounts/", include("accounts.urls")),
 ]
